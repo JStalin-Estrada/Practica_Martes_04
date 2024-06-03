53a6c48851fc86b98e06a9f717a32b8f2336470e
ktrianaa2 <ktrianaa2@uteq.edu.ec>
2024-06-03 12:32:24 -0500

    Initial commit

---
+++ app/src/main/java/com/example/practica_lunes_03/Lista_de_usuario.java
@@ -0,0 +1,34 @@
+package com.example.practica_lunes_03;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import org.json.JSONException;
+
+import WebServices.Asynchtask;
+
+public class Lista_de_usuario extends AppCompatActivity
+        implements Asynchtask {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_lista_de_usuario);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+
+    @Override
+    public void processFinish(String result) throws JSONException {
+
+    }
+}
---
+++ app/src/main/java/com/example/practica_lunes_03/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.practica_lunes_03;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import org.json.JSONException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import WebServices.Asynchtask;
+import WebServices.WebService;
+
+public class MainActivity extends AppCompatActivity
+                    implements Asynchtask {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+public void clickLogin(View v){
+    Map<String, String> datos = new HashMap<String, String>();
+    EditText txtCedula = findViewById(R.id.txtCedula);
+
+    EditText txtClave = findViewById(R.id.txtClave);
+    String pass= txtClave.getText().toString();
+    String usr= txtCedula.getText().toString();
+    WebService ws = new WebService(
+            "https://revistas.uteq.edu.ec/ws/login.php?" + "usr=" + usr + "&pass==" + pass,
+            datos, MainActivity.this, MainActivity.this);
+    ws.execute("GET");
+}
+
+
+    @Override
+    public void processFinish(String result) throws JSONException {
+        Toast.makeText(this, result, Toast.LENGTH_LONG).show();
+
+    }
+}
